6574d5854fe6454621c9340fa5f6b13bd7dca388
vitu6569 <victoralmeida6569@gmail.com>
2024-09-02 22:01:53 -0300

    new update :tada:

--- src/components/common/toggle.jsx
+++ src/components/common/toggle.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 function Toggle() {
+    function handleClick() {
+        const switchCont = document.getElementById("switch");
+        const html = document.documentElement;
+        const currentTheme = html.classList.contains("light") ? "dark" : "light";
+        
+        localStorage.setItem("theme", currentTheme);
+        html.className = currentTheme;
+    
+        if (currentTheme === "light") {
+            switchCont.style.animation = "fall-in 1.5s forwards";
+        } else {
+            switchCont.style.animation = "fall-back 1.5s forwards";
+        }
+      }
+
     return (
         <div id="switch">
-            <button>
+            <button onClick={handleClick}>
 
             </button>
             <span></span>
