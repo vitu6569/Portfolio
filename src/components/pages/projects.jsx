f7bab978e73c8301de971afe6aa0b29ee768b927
vitu6569 <victoralmeida6569@gmail.com>
2024-10-02 15:07:33 -0300

    new update :tada:

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from '../common/Header'
 import Menu from '../common/menu'
+import { Link } from "react-router-dom";
+
+const projects = [1, 2, 3, 4, 5]
 
 function Projects() {
     return (
         <div>
             <div><Header/></div>
             <div><Menu/></div>
+            <div className="flex flex-col gap-2">
+                {projects.map((project) => (
+                    <Link key={project} to={`/projects/${project}`}>
+                        project {project}
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
