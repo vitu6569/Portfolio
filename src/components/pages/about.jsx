770c7eacaf50b4014a1a77850a92e7e0464f6374
vitu6569 <victoralmeida6569@gmail.com>
2024-10-03 17:37:23 -0300

    New update :tada:

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -6,15 +6,21 @@ function About() {
     return (
         <div>
             <div><Header/></div>
-            <div><Menu/></div>
-            <div id="container">
-                <div id="aboutContent">
-                    <h1 id="aboutTitle">A<span id="transparentB">B</span>OUT</h1>
-                    <p id="aboutResume">Hi, my name is João Victor and i use Vitu as my nickname across social medias. I'm a web developer and 3D artist from Brazil. I also love listening to music and I’m always curious to learn more when it comes to new technologies and creative coding.</p>
-                </div>
-            </div>
+            <div><Menu/></div> 
+            <main id="containerAbout">
+                <section id="secondContainer">
+                    <div id="leftContainer">
+                        <h1 id="introductionTitleAbout">A<span id="transparentB">B</span>OUT</h1>
+                        <p id="aboutResume">Hi, my name is João Victor and i use Vitu as my nickname across social medias. I'm a Front-end developer and 3D artist from Brazil. I also love listening to music and I’m always curious to learn more when it comes to new technologies and creative coding.</p>
+                    </div>
+                </section>
+            </main>
         </div>
     )
+    /*
+        <h1 id="aboutTitle">A<span id="transparentB">B</span>OUT</h1>
+        <p id="aboutResume">Hi, my name is João Victor and i use Vitu as my nickname across social medias. I'm a Front-end developer and 3D artist from Brazil. I also love listening to music and I’m always curious to learn more when it comes to new technologies and creative coding.</p>
+    */
 }
 
 export default About;
