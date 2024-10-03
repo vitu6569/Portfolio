815025cd4fff4655381c8cbc72749465cb53a150
vitu6569 <victoralmeida6569@gmail.com>
2024-10-03 13:35:00 -0300

    New update :tada:

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -7,6 +7,12 @@ function About() {
         <div>
             <div><Header/></div>
             <div><Menu/></div>
+            <div id="container">
+                <div id="aboutContent">
+                    <h1 id="aboutTitle">A<span id="transparentB">B</span>OUT</h1>
+                    <p id="aboutResume">Hi, my name is João Victor and i use Vitu as my nickname across social medias. I'm a web developer and 3D artist from Brazil. I also love listening to music and I’m always curious to learn more when it comes to new technologies and creative coding.</p>
+                </div>
+            </div>
         </div>
     )
 }
