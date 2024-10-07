34f293326e4f710de481f852e6c2f3076bd06bb8
vitu6569 <victoralmeida6569@gmail.com>
2024-10-06 22:12:32 -0300

    new update :tada:

--- src/components/pages/contacts.jsx
+++ src/components/pages/contacts.jsx
@@ -7,6 +7,21 @@ function Contact() {
         <div>
             <div><Header/></div>
             <div><Menu/></div>
+            <main id="containerContacts">
+                <section id="secondContainer">
+                        <div id="leftContainer">
+                            <h1 id="introductionTitleContacts">C<span id="transparentO">o</span>ntacts</h1>
+                            <div id="contacts">
+                                <div>
+                                    <div className="nameCont" ><p id="mail">mail</p><a href="mailto:victoralmeida6569@outlook.com" className="linkCont">victoralmeida6569@outlook.com</a></div>
+                                </div>
+                                <div>
+                                    <p className="nameCont"><a href="https://www.instagram.com/svictpr/">Instagram</a></p>
+                                </div>
+                            </div>
+                        </div>
+                </section>
+            </main>
         </div>
     )
 }
